14d096ce321f937bd0739c58b6c01a5a494d7f83
Kevin Edwards <kev@Macintosh-2.lan>
2009-06-26 12:52:36 +0100

    draw a node

--- ChemDraw/Classes/DrawView.m
+++ ChemDraw/Classes/DrawView.m
@@ -41,8 +41,35 @@
 	
     CGContextShowTextAtPoint(ctx, 15, 50, text, strlen(text));
 	
+    	
 
 }
+
+- (void) drawNode {
+	
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	CGContextSetRGBFillColor(ctx, 255, 0, 0, 1.0);
+    CGContextFillEllipseInRect(ctx, CGRectMake(5, 10, 10, 10));
+	return;
+	
+}
+
+-(CGPathRef) pathInRect:(CGRect)rect { 
+	CGMutablePathRef path = CGPathCreateMutable(); 
+	CGFloat radius = CGRectGetHeight(rect) / 2.0f; 
+	CGPathMoveToPoint(path, NULL, CGRectGetMinX(rect), CGRectGetMinY(rect)); 
+	CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rect) - radius, 
+						 CGRectGetMinY(rect)); 
+	CGPathAddArc(path, NULL, CGRectGetMaxX(rect) - radius, 
+				 CGRectGetMinY(rect) + radius, 
+				 radius, -M_PI, M_PI, NO); 
+	CGPathAddLineToPoint(path, NULL, CGRectGetMinX(rect), CGRectGetMaxY(rect)); 
+	CGPathCloseSubpath(path); 
+	CGPathRef imutablePath = CGPathCreateCopy(path); 
+	CGPathRelease(path); 
+	return imutablePath; 
+} 
+
 
 
 - (void)dealloc {
