7c83db24792ee08c28272a284b9fa6ade0fd7842
Kevin Edwards <kev@Macintosh-2.lan>
2009-06-26 12:40:25 +0100

    drew text to screen

--- ChemDraw/Classes/DrawView.m
+++ ChemDraw/Classes/DrawView.m
@@ -22,8 +22,25 @@
 
 - (void)drawRect:(CGRect)rect {
     // Drawing code
-	[[UIColor blueColor] setFill]; 
-	UIRectFill(rect); 
+	[[UIColor whiteColor] setFill]; 
+	UIRectFill(rect);
+	
+	// got the graphics context
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	
+	char* text = "TrailsintheSand.com";
+    CGContextSelectFont(ctx, "Helvetica", 16.0, kCGEncodingMacRoman);
+    CGContextSetTextDrawingMode(ctx, kCGTextFill);
+    CGContextSetRGBFillColor(ctx, 0, 0, 0, 1);
+	
+    CGAffineTransform xform = CGAffineTransformMake(
+													1.0,  0.0,
+													0.0, -1.0,
+													0.0,  0.0);
+    CGContextSetTextMatrix(ctx, xform);
+	
+    CGContextShowTextAtPoint(ctx, 80, 50, text, strlen(text));
+	
 
 }
 
